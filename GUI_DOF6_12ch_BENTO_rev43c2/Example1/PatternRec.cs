6598b1c78271bd0358e1efc5c9e3015f18d97823
Eric Wells <ewells@ualberta.ca>
2020-11-20 14:37:10 -0700

    added selectable input features for data logging, added seperate data logger for pattern rec tab, added inputdevicedata variable to store all inputs as floats for more continuous data logging of raw values, fixed a few copy-pastes in the input mapping update section with loops

---
+++ GUI_DOF6_12ch_BENTO_rev43c2/Example1/PatternRec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace brachIOplexus
+{
+    class PatternRec
+    {
+        int windowlength = 100;
+
+        public PatternRec()
+        {
+            //Matrix<float> A = Matrix<float>.Build.Random(3, 4);
+            //A.SubMatrix
+        }
+
+        public float[] MAV(float[] data, float[] timestamps, int windowlength)
+        {
+            int datalen = data.Length;
+            float[] MAVdata = data;
+
+            return MAVdata;
+
+        }
+    }
+}

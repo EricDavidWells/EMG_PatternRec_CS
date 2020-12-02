66973851b28f9343d9037d7a795ced2df1e5c43a
Eric Wells <ewells@ualberta.ca>
2020-12-01 19:44:08 -0700

    fixed brachIOplexus delay bug by increasing accuracy of stopWatch1 and removing unnecessary thread.sleep, updated data logging in main loop, added a few helper functions for data logging timestamps, updated helper functions for grabbing all selected input device data, implemented relax time contract time and cycle numbers

--- GUI_DOF6_12ch_BENTO_rev43c2/Example1/PatternRec.cs
+++ GUI_DOF6_12ch_BENTO_rev43c2/Example1/PatternRec.cs
@@ -5,13 +5,26 @@ using System.Text;
 using System.IO;                // For save/open parameters functionality
 using MathNet.Numerics.LinearAlgebra;
 
+
+// useful: https://dotnetfiddle.net/
+
 namespace brachIOplexus
 {
     class PatternRec
     {
         int windowlength = 100;
-        int current_output_label = 0;
         public dataLogger logger = new dataLogger();
+        public int current_output = 0;
+        public string[] output_labels;
+        public int output_num;
+        public int contraction_time = 1000;
+        public int relax_time = 3000;
+        public long start_time = 0;
+        public int collection_cycles = 1;
+        public int current_cycle = 0;
+
+        public bool trainFlag = false;  // flag to indicate training has begun
+        public bool contractFlag = false;
 
         public PatternRec()
         {
@@ -39,6 +52,78 @@ namespace brachIOplexus
             {
 
             }
+        }
+
+        public void write_data_with_output(string[] data)
+        {
+            /*
+            appends the current output label to the data and sends data to data logger
+            */
+            string[] temp = new string[data.Length + 1];
+            temp[data.Length] = current_output.ToString();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                temp[i] = data[i];
+            }
+            logger.write_data(temp);
+        }
+
+        public void write_header_with_output(string[] data)
+        {
+            /*
+            appends the current output label to the header and sends data to data logger
+            */
+            string[] temp = new string[data.Length + 1];
+            temp[data.Length] = "output";
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                temp[i] = data[i];
+            }
+            logger.write_header(temp);
+        }
+
+        public void set_outputs(string[] outputs_)
+        {
+            output_labels = outputs_;
+            output_num = outputs_.Length;
+        }
+
+        public void tick()
+        {
+            logger.tick();
+            if (trainFlag)
+            {
+                long elapsed_time = logger.curtime - start_time;
+                long segment_time = relax_time + contraction_time;
+
+                current_output = (int)Math.Floor((decimal)elapsed_time / segment_time);
+
+                long local_time = elapsed_time - segment_time * current_output;
+                contractFlag = local_time >= relax_time;
+
+                current_cycle = (int)Math.Floor((decimal)elapsed_time / (segment_time*output_num));
+
+                if (current_cycle >= collection_cycles)
+                {
+                    trainFlag = false;
+                    logger.file.Flush();
+                }
+            }
+        }
+
+        public void start_train()
+        {
+            logger.init_logger(logger.filepath);
+            trainFlag = true;
+            logger.tick();
+            start_time = logger.curtime;
+        }
+
+        public void end_train()
+        {
+            trainFlag = false;
         }
     }
 }

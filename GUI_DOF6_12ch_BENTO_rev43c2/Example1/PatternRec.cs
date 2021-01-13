6cf8602297fec53d3b50d4b200da2364ce05fe60
Rico5678 <ewells@ualberta.ca>
2021-01-12 17:56:24 -0700

    updated MAV value function to be overloaded with only a List<float> input version, this allows it to be easily added to a pipeline, and all features selected can be added easily, created functions to get desired feature mappings and add them to the pipeline, two pipelines are used, one for EMG data and one for generic data, also added ZeroCrossings function and Slope Sign Changes function, also changed LoadFileToListCols function to load data into a slightly different list data structure, where each list inside the inputs list is a single data signal at all time stamps

--- GUI_DOF6_12ch_BENTO_rev43c2/Example1/PatternRec.cs
+++ GUI_DOF6_12ch_BENTO_rev43c2/Example1/PatternRec.cs
@@ -14,6 +14,7 @@ using Accord.Statistics.Distributions.Univariate;
 using Accord.MachineLearning.VectorMachines.Learning;
 using Accord.Statistics.Kernels;
 
+
 // useful: https://dotnetfiddle.net/
 // potential library: http://accord-framework.net/
 
@@ -39,23 +40,32 @@ namespace brachIOplexus
         public int output_num;
         public int input_num;
 
-        public int model_update_freq;
+        public int model_update_freq = 50;
+        public int model_window_time = 100;
+        public int model_window_overlap;
+
         public bool modelFlag = false;  // keeps track of whether a model is loaded
 
-        public List<float> timestamps;
-        public List<List<float>> inputs;
-        public List<int> outputs;
-        public List<int> feature_types;     // keep track of feature type, 0 = generic, 1 = EMG
+        public List<float> timestamps = new List<float>();
+        public List<List<float>> inputs = new List<List<float>>();
+        public List<List<float>> features = new List<List<float>>();
+        public List<int> outputs = new List<int>();
+        public List<int> feature_outputs = new List<int>();
+        public List<int> input_types = new List<int>();     // keep track of feature type, 0 = generic, 1 = EMG
+
+        public List<Func<List<float>, List<float>>> emg_pipeline = new List<Func<List<float>, List<float>>>();
+        public List<Func<List<float>, List<float>>> generic_pipeline = new List<Func<List<float>, List<float>>>();
 
         public PatternRec()
         {
             //Matrix<float> A = Matrix<float>.Build.Random(3, 4);
             //A.SubMatrix
+            //Accord.Math.Matrix.Get()
         }
 
-        void MAV(List<float> raw_values, float window_time, float freq)
+        public List<float> MAV(List<float> raw_values, float window_time, float freq)
         {
-            // should I use timestamps or have consistent window length??????????? 
+            // returns list of Mean Absolute Value features
 
             List<float> filtered_values = new List<float>();
             int window_n = (int)Math.Ceiling(window_time * freq / 1000);
@@ -71,9 +81,53 @@ namespace brachIOplexus
                 }
                 filtered_values.Add(sum / window_n);
             }
+            return filtered_values;
         }
 
-        public bool LoadFileToList(string filepath)
+        public List<float> MAV(List<float> raw_values)
+            // overloaded method to eliminate window time and requency input parameters
+        {
+            return MAV(raw_values, model_window_time, model_update_freq);
+        }
+
+        List<float> ZeroCrossings(List<float> raw_values, float window_time, float freq)
+        {
+            // returns list of Zero Crossings features
+            List<float> filtered_values = new List<float>();
+            int window_n = (int)Math.Ceiling(window_time * freq / 1000);
+
+            for (int i = 0; i < (raw_values.Count - window_n); i++)
+            {
+                List<float> window = raw_values.GetRange(i, window_n);
+
+                int crossing_count = 0;
+                bool positive_flag = (window[0] >= 0);    // true for 
+
+                foreach (float value in window)
+                {
+                    if ((value >= 0) != positive_flag)
+                    {
+                        crossing_count++;
+                        positive_flag = !positive_flag;
+                    }
+                }
+                filtered_values.Add(crossing_count);
+            }
+
+            return filtered_values;
+        }
+
+        List<float> SSC(List<float> raw_values, float window_time, float freq)
+        {
+            // returns list of Slope Sign Change features
+            int window_n = (int)Math.Ceiling(window_time * freq / 1000);
+
+            List<float> difference_values = raw_values.Zip(raw_values.Skip(1), (x, y) => y-x).ToList();
+            List<float> filtered_values = ZeroCrossings(difference_values, windowlength, freq);
+            return filtered_values;
+        }
+
+        public bool LoadFileToListRows(string filepath)
         {
             // read data file into lists inputs and outputs, return false if unsuccessful
 
@@ -100,6 +154,18 @@ namespace brachIOplexus
                     {
                         input_labels.RemoveAt(input_labels.Count - 1);
                         input_labels.RemoveAt(0);
+
+                        for (int i=0; i<input_labels.Count; i++)
+                        {
+                            if (input_labels[i].ToLower().Contains("ch"))
+                            {
+                                input_types.Add(1);
+                            }
+                            else
+                            {
+                                input_types.Add(0);
+                            }
+                        }
                     }
                 }
                 // parse data lines
@@ -126,88 +192,130 @@ namespace brachIOplexus
             return true;
         }
 
-        //public bool LoadFile(string filepath)
-        //{
+        public bool LoadFileToListCols(string filepath)
+        {
+            // read data file into lists inputs and outputs, return false if unsuccessful
 
-        //    string[] lines = System.IO.File.ReadAllLines(filepath);
-        //    num_cols = lines[0].Split(',').Length - 1; // number of data columns in the file (-1 for line label)
-        //    num_lines = lines.Length - 1;    // number of lines in the data file (-1 for header)
+            string[] lines_arr = System.IO.File.ReadAllLines(filepath);
+            List<string> lines = lines_arr.ToList();
 
-        //    // initialize raw data jagged array
-        //    raw_data = new float[num_cols][];
-        //    for (int i = 0; i < num_cols; i++)
-        //    {
-        //        raw_data[i] = new float[num_lines];
-        //    }
+            int input_num = lines[0].Split(',').Length - 3;
 
-        //    // ensure header is first line in file
-        //    string headerline = lines[0];
-        //    if (headerline[0] != 'h')
-        //    {
-        //        return false;
-        //    }
 
-        //    // parse header line
-        //    char[] chars_to_trim = { 'd', 'h', ',' };
-        //    headerline = headerline.TrimStart(chars_to_trim);
-        //    raw_data_labels = headerline.Split(',');
+            inputs.Clear();
+            outputs.Clear();
+            timestamps.Clear();
+            input_types.Clear();
 
-        //    // parse data lines
-        //    for (int i=0; i<num_lines-1; i++)
-        //    {
-        //        string line = lines[i+1];   // skip first line
-                
-        //        // ensure all lines have data stamp as first index
-        //        if (line[0] != 'd')
-        //        {
-        //            return false;
-        //        }
+            // preallocate inputs
+            for (int i = 0; i < input_num; i++)
+            {
+                inputs.Add(new List<float>());
+            }
 
-        //        line = line.TrimStart(chars_to_trim);
-        //        string[] vals = line.Split(',');
-                
-        //        for (int j=0; j<num_cols; j++)
-        //        {
-        //            raw_data[j][i] = float.Parse(vals[j]);
-        //        }
-                
-        //    }
+            foreach (string line in lines)
+            {
+                // parse header line
+                if (line[0] == 'h')
+                {
+                    char[] chars_to_trim = { 'h', ',' };
+                    input_labels = line.TrimStart(chars_to_trim).Split(',').ToList<string>();
 
-        //    // split up into input and output data arrays
-        //    input_data = new float[num_cols-2][];
-        //    for (int i=0; i < num_cols - 2; i++)
-        //    {
-        //        input_data[i] = new float[num_lines];
-        //        input_data[i] = raw_data[i + 1];
-        //    }
+                    if (input_labels.Last() != "output")
+                    {
+                        return false;  // return if there is no output column
+                    }
+                    else
+                    {
+                        input_labels.RemoveAt(input_labels.Count - 1);
+                        input_labels.RemoveAt(0);
 
-        //    output_data = new int[num_lines];
-        //    for (int i=0; i < num_lines; i++)
-        //    {
-        //        output_data[i] = (int)raw_data[num_cols-1][i];
-        //    }
+                        for (int i = 0; i < input_labels.Count; i++)
+                        {
+                            if (input_labels[i].ToLower().Contains("ch"))
+                            {
+                                input_types.Add(1);
+                            }
+                            else
+                            {
+                                input_types.Add(0);
+                            }
+                        }
+                    }
+                }
+                // parse data lines
+                else if (line[0] == 'd')
+                {
+                    char[] chars_to_trim = { 'd', ',' };
+                    List<float> vals = line.TrimStart(chars_to_trim).Split(',').ToSingle().ToList<float>();
+                    outputs.Add((int)vals.Last());
+                    vals.RemoveAt(vals.Count - 1);
 
-        //    return true;
-        //}
+                    timestamps.Add(vals.First());
+                    vals.RemoveAt(0);
+
+                    for (int i=0; i<vals.Count; i++)
+                    {
+                        inputs[i].Add(vals[i]);
+                    }
+                }
+                else
+                {
+                    return false; // return if there is no leading character (i.e. not a proper data file)
+                }
+            }
+
+            // find frequency used for logging
+            model_update_freq = (int)Math.Round(timestamps.Count * 1000 / (timestamps.Last() - timestamps.First()));
+            return true;
+        }
+
+        public void map_features(List<List<float>> data)
+        {
+
+            features.Clear();
+            feature_outputs.Clear();
+            int new_feature_size = 0;
+
+            foreach(Func<List<float>, List<float>> f in generic_pipeline)
+            {
+                foreach(List<float> input in data)
+                {
+                    features.Add(f(input));
+                    new_feature_size = features[0].Count();
+                }
+            }
+
+            // trim outputs due to windowing of features
+            feature_outputs.AddRange(outputs.GetRange(0, new_feature_size));
+        }
 
         public void train_model_Accord()
         {
-            double[][] inputs =
-            {
-                new double[] { 0 },
-                new double[] { 3 },
-                new double[] { 1 },
-                new double[] { 2 },
-            };
+            float[][] inputs = features.Select(a => a.ToArray()).ToArray();
 
-            // Outputs for each of the inputs
-            int[] outputs =
-            {
-                0,
-                3,
-                1,
-                2,
-            };
+            double[][] inputs_dub = Array.ConvertAll(inputs, x => Array.ConvertAll(x, y => (double)y));
+
+            int n = inputs.Length;
+            outputs.RemoveRange(n, outputs.Count - n);
+            
+            
+            //outputs.RemoveRange(outputs.cou, 3);
+            //{
+            //    new double[] { 0 },
+            //    new double[] { 3 },
+            //    new double[] { 1 },
+            //    new double[] { 2 },
+            //};
+
+            //// Outputs for each of the inputs
+            //int[] outputs =
+            //{
+            //    0,
+            //    3,
+            //    1,
+            //    2,
+            //};
 
 
             // Create the Multi-label learning algorithm for the machine
@@ -220,66 +328,12 @@ namespace brachIOplexus
             };
 
             // Learn a multi-label SVM using the teacher
-            var svm = teacher.Learn(inputs, outputs);
+            var svm = teacher.Learn(inputs_dub, outputs.ToArray());
 
             // Compute the machine answers for the inputs
-            int[] answers = svm.Decide(inputs);
+            int[] answers = svm.Decide(inputs_dub);
+
         }
-
-        //public void train_model()
-        //{
-        //    // example from cookbook on github: https://github.com/dotnet/machinelearning/blob/master/docs/code/MlNetCookBook.md%23how-do-i-load-data-from-a-text-file
-        //    var context = new MLContext();
-
-        //    string trainDataPath = @"C:\Users\Rico5678\Desktop\garbage data\20210108_123341_keyboarddata.csv";
-
-
-        //    //// Load the data into a data view, class based method
-        //    //var data = context.Data.LoadFromTextFile<RegressionData>(trainDataPath,
-        //    //                // Default separator is tab, but we need a semicolon.
-        //    //                separatorChar: ',',
-        //    //                // First line of the file is a header, not a data row.
-        //    //                hasHeader: true
-        //    //);
-
-        //    var loader = context.Data.CreateTextLoader(new[] {
-        //            // A boolean column depicting the 'label'.
-        //            new TextLoader.Column("Features", DataKind.Single, 1, num_cols-1),
-        //            // Three text columns.
-        //            new TextLoader.Column("Label", DataKind.UInt16, num_cols),
-        //        },
-        //        // First line of the file is a header, not a data row.
-        //        hasHeader: true, 
-        //        separatorChar: ','
-        //    );
-
-        //    var data = loader.Load(trainDataPath);
-
-        //    var pipeline = context.Transforms.Conversion.MapValueToKey("Label").Append(context.MulticlassClassification.Trainers.SdcaMaximumEntropy());
-        //    //var temp = data.Preview();
-        //    var split = context.Data.TrainTestSplit(data, testFraction: 0.1);
-        //    var model = pipeline.Fit(split.TrainSet);
-        //    var metrics = context.MulticlassClassification.Evaluate(model.Transform(split.TestSet));
-
-        //    // make a prediction:
-
-        //    var schemaDef = SchemaDefinition.Create(typeof(ClassificationVectorData));
-        //    schemaDef["Features"].ColumnType = new VectorDataViewType(NumberDataViewType.Single, num_cols - 1);
-
-        //    var temp__ = new ClassificationVectorData();
-        //    temp__.Features = new float[] { 1, 0, 0, 500 };
-
-        //    //var predictionFunc = context.Model.CreatePredictionEngine<ClassificationVectorData, ClassificationPrediction>(model, schemaDef);
-
-
-        //    //var prediction = predictionFunc.Predict(temp__);
-
-        //    //var dataViewBuilder = new DataViewSchema.Builder();
-        //    //dataViewBuilder.AddColumn("Feature")
-
-
-        //}
-
 
         public void update_data_with_output(string[] data)
         {
@@ -361,33 +415,6 @@ namespace brachIOplexus
             logger.file.Flush();
             logger.close();
         }
-    }
-
-    class RegressionData
-    {
-
-        [LoadColumn(1), ColumnName("Features")]
-        public float FeatureVector { get; set; }
-
-
-        [LoadColumn(2), ColumnName("Label")]
-        public uint Target { get; set; }
-    }
-
-    class ClassificationVectorData
-    {
-        [ColumnName("Features")]
-        public float[] Features { get; set; }
-
-        [ColumnName("Label")]
-        public UInt16 Label { get; set;}
-
-    }
-
-    class ClassificationPrediction
-    {
-        [ColumnName("Label")]
-        public UInt16 PredictedClass { get; set; }
     }
 }
 
